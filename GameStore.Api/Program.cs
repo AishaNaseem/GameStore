50553910585d24bdd42b5d6c4597ee8461e9bd00
Aisha Naseem <aishanaseemjm@gmail.com>
2023-06-02 17:37:07 -0400

    Implementing an Entity Framework repository

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,10 +1,13 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IGamesRepository, InMemGamesRepository>();
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
-
+builder.Services.AddSqlServer<GameStoreContext>(connString);
 var app = builder.Build();
+app.Services.InitializeDb();
 app.MapGamesEndPoints();
 app.Run();
